a71f9025732eaf763c5ef069260004682d24620b
Javier Martínez <erick97am@gmail.com>
2025-10-16 18:30:38 -0600

    Use Tailwindcss
    As recomended  by Nextjs

--- app/layout.js
+++ app/layout.js
@@ -23,10 +23,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
-          <Link className="link" href="/productos">Productos</Link>
-          <Link className="link" href="/servicios">Servicios</Link>
-          <Link className="link" href="/buscar">Buscar...</Link>
+        <nav className="flex flex-row font-bold gap-2 pt-2 pb-2 pr-2 pl-2 text-size-lg border-b-1">
+          <Link className="p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100" href="/">Inicio</Link>
+          <Link className="p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100" href="/productos">Productos</Link>
+          <Link className="p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100" href="/servicios">Servicios</Link>
+          <Link className="p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100" href="/buscar">Buscar...</Link>
         </nav>
         {children}
       </body>
