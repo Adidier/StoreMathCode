555cd57fe7ec9aab4fca98526d4df264ce2f1526
Javier Martínez <erick97am@gmail.com>
2025-10-17 11:18:41 -0600

    Better display of prices

--- app/ui/CatalogView.jsx
+++ app/ui/CatalogView.jsx
@@ -13,7 +13,7 @@ export function CatalogView({
                         <h2 className="text-xl font-bold mb-2">{item.name}</h2>
                         {/* TODO: Seguramente se van a ocupar todas las imagenes del mismo tamaño */}
                         <Image src={item.images[0]} alt={item.name} width={427} height={427} className="object-cover rounded-md mb-2" />
-                        <h3 className="text-lg  font-bold p-1">${fetchPrice(item.default_price)}</h3>
+                        <h3 className="text-lg  font-bold p-1">{fetchPrice(item.default_price)}</h3>
                         <p className="truncate">{item.description}</p>
                         <AddToCartBtn item={item} />
                     </div>
