9c1cbd65ed352f6109a965d4d7e37c01ce2b9aa1
Javier Martínez <erick97am@gmail.com>
2025-10-16 19:13:37 -0600

    Add Shopping Cart Context
    At this point is still missing the ability to save the cart between reloads.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./ui/globals.css";
 import { NavBar } from "./ui/NavBar";
+import { Providers } from "./context/Providers"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        {children}
+        <Providers>
+          <NavBar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
