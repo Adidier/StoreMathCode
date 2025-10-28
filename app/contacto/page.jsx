ba1ac90dd62044f61862627f0f2fe3e67b0674eb
Javier Martínez <erick97am@gmail.com>
2025-10-28 13:12:33 -0600

    Add contact page

---
+++ app/contacto/page.jsx
@@ -0,0 +1,13 @@
+import { Footer } from "../ui/Footer"
+import { ContactForm } from "../ui/ContactForm"
+
+export default function Contacto() {
+    return(
+        <>
+         <main>
+            <ContactForm/>
+        </main>
+        <Footer />
+        </>
+    )
+}
