dd3c7160e710793968da0f8f3c560c30eab31835
Javier Martínez <erick97am@gmail.com>
2025-10-28 13:01:46 -0600

    Add better navbar header

--- app/ui/NavBar.jsx
+++ app/ui/NavBar.jsx
@@ -1,17 +1,32 @@
-"use client";
-
-import { usePathname } from "next/navigation";
+import Image from "next/image";
 import Link from "next/link";
 
 export function NavBar() {
-  const pathname = usePathname();
   return (
-    <nav className="flex flex-row font-bold gap-2 pt-2 pb-2 pr-2 pl-2 text-size-lg border-b-1">
-      <Link className="p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100" href="/">Inicio</Link>
-      <Link className={`p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100 ${pathname === '/productos' ? 'bg-neutral-900 text-gray-100' : ''}`} href="/productos">Productos</Link>
-      <Link className={`p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100 ${pathname === '/servicios' ? 'bg-neutral-900 text-gray-100' : ''}`} href="/servicios">Servicios</Link>
-      <Link className={`p-1.5 rounded-lg hover:bg-neutral-900 hover:text-gray-100 ${pathname === '/buscar' ? 'bg-neutral-900 text-gray-100' : ''}`} href="/buscar">Buscar...</Link>
+    <nav className="flex flex-col bg-stone-50 sticky top-0 z-10 shadow-md">
+      {/* <div className="grid grid-cols-2 font-light text-xs pt-2 pb-2 bg-sky-600 text-white p-2">
+                <div className="justify-self-center flex gap-7">
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/faq">PREGUNTAS FREQUENTES</Link>
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">TUTORIALES</Link>
+                </div>
+                <div className="justify-self-center flex gap-7">
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/portal">PORTAL</Link>
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">SUBIR COMPROBANTE</Link>
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">ESTATUS</Link>
+                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">PAGAR</Link>
+                </div>
+            </div> */}
+      <div className="flex flex-row gap-9 justify-center items-center font-light text-sm">
+        <Image src="/logoFlashback.webp" alt="Logo" width={253} height={91} className="ml-8 mr-8 mt-1.5 mb-1.5" />
+        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">INICIO</Link>
+        {/* <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/nosotros">NOSOTROS</Link> */}
+        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
+        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/servicios">SERVICIOS</Link>
+        {/* <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">EVENTOS SOCIALES</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/galeria">GALERIA</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">TIENDA</Link> */}
+        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
+      </div>
     </nav>
   );
 }
-
