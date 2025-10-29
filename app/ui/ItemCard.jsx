8ed38886c24625d665229c78e49b9c1850e8aeaa
Javier Martínez <erick97am@gmail.com>
2025-10-28 18:57:49 -0600

    Fix layout of 'Productos' and 'Servicios' pages

--- app/ui/ItemCard.jsx
+++ app/ui/ItemCard.jsx
@@ -6,7 +6,12 @@ export function ItemCard({ ...props }) {
     const { item } = props;
     return (
         <div key={item.d} className="flex flex-col bg-gray-50 rounded-b-md shadow hover:shadow-md">
-            <Image src={item.images[0]} alt={item.name} width={427} height={427} className="object-cover rounded-t-md mb-2" />
+            <Image
+                src={item.images[0]}
+                alt={item.name}
+                width={300} 
+                height={300}
+                className="object-cover rounded-t-md mb-2" />
             <div className="flex flex-col h-full justify-end p-1">
                 <p className="text-xl font-bold mb-2">{item.name}</p>
                 <p className="text-lg font-bold p-1">{fetchPrice(item.default_price)}</p>
