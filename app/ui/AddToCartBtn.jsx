612b829cc513864ada3c1759f31eac69e131024c
Javier Martínez <erick97am@gmail.com>
2025-10-28 18:40:59 -0600

    Improve ItemCard

--- app/ui/AddToCartBtn.jsx
+++ app/ui/AddToCartBtn.jsx
@@ -6,8 +6,9 @@ export function AddToCartBtn({ item }) {
     const cart = useCart()
     return (
         <div>
-            <button className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-blue-700" onClick={() => { cart.addItem(item) }
-            }>Add to cart</button>
+            <button className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-blue-700"
+                onClick={() => { cart.addItem(item) }
+                }>Add to cart</button>
         </div>
     )
 }
--- app/ui/ItemCard.jsx
+++ app/ui/ItemCard.jsx
@@ -5,12 +5,14 @@ import Image from "next/image";
 export function ItemCard({ ...props }) {
     const { item } = props;
     return (
-        <div key={item.d} className="flex flex-col">
-            <Image src={item.images[0]} alt={item.name} width={427} height={427} className="object-cover rounded-md mb-2" />
-            <h2 className="text-xl font-bold mb-2">{item.name}</h2>
-            <h3 className="text-lg  font-bold p-1">{fetchPrice(item.default_price)}</h3>
-            <p className="truncate">{item.description}</p>
-            <AddToCartBtn item={item} />
+        <div key={item.d} className="flex flex-col bg-gray-50 rounded-b-md shadow hover:shadow-md">
+            <Image src={item.images[0]} alt={item.name} width={427} height={427} className="object-cover rounded-t-md mb-2" />
+            <div className="flex flex-col h-full justify-end p-1">
+                <p className="text-xl font-bold mb-2">{item.name}</p>
+                <p className="text-lg font-bold p-1">{fetchPrice(item.default_price)}</p>
+                {/* <p className="truncate">{item.description}</p> */}
+                <AddToCartBtn item={item} />
+            </div>
         </div>
     )
 }
