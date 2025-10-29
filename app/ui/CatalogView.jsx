583882228c4089ba3d2784930599e4976a6b8c1f
Javier Martínez <erick97am@gmail.com>
2025-10-28 18:04:43 -0600

    Simplify CatalogwView
    Create new ItemCard component

--- app/ui/CatalogView.jsx
+++ app/ui/CatalogView.jsx
@@ -1,6 +1,4 @@
-import { fetchPrice } from "@/lib/data";
-import { AddToCartBtn } from "@/app/ui/AddToCartBtn";
-import Image from "next/image";
+import { ItemCard } from "./ItemCard";
 
 export function CatalogView({
     data
@@ -9,14 +7,7 @@ export function CatalogView({
         <div className="flex flex-col m-3 gap-1 place-self-center md:gap-2.5 md:m-10 md:grid md:grid-cols-3 lg:grid-cols-4">
             {
                 data.map((item) => (
-                    <div key={item.id} className="flex flex-col p-4 rounded-md border-1 border-gray-400 gap-3 shadow md:max-w-lg">
-                        <h2 className="text-xl font-bold mb-2">{item.name}</h2>
-                        {/* TODO: Seguramente se van a ocupar todas las imagenes del mismo tamaño */}
-                        <Image src={item.images[0]} alt={item.name} width={427} height={427} className="object-cover rounded-md mb-2" />
-                        <h3 className="text-lg  font-bold p-1">{fetchPrice(item.default_price)}</h3>
-                        <p className="truncate">{item.description}</p>
-                        <AddToCartBtn item={item} />
-                    </div>
+                    <ItemCard key={item.id} item={item} />
                 ))
             }
         </div>
