f327586657fb4d9e07a2a87f3eb918522d2d2bd7
Javier Martínez <erick97am@gmail.com>
2025-10-30 16:36:04 -0600

    Better layout for cart page

--- app/carrito/page.jsx
+++ app/carrito/page.jsx
@@ -11,7 +11,7 @@ export default function Cart() {
     if (cartContext.cart.length === 0) {
         return (
             <>
-                <main className="grid justify-items-center items-center pb-20 gap-16 sm:p-20 overflow-x-hidden">
+                <main className="grid justify-items-center items-center pb-20 gap-16 sm:p-20 overflow-x-hidden bg-gray-200">
                     <p className="rounded-md bg-gray-50 p-5 text-xl font-bold shadow-md">El carrito de compras esta vacio</p>
                     <Link className="p-2 text-gray-50 shadow-md bg-red-600 rounded-lg" href="/productos">Revisa nuestros productos</Link>
                 </main>
@@ -21,40 +21,56 @@ export default function Cart() {
     } else {
         return (
             <>
-                <main className="flex flex-col m-3 gap-3 place-self-center">
-                    {
-                        cartContext.cart.map((item) => (
-                            <div key={item.id} className="flex flex-row rounded-md gap-3 border border-gray-400 items-start shadow max-w-lg">
-                                <Image
-                                    src={item.images[0]}
-                                    alt={item.name}
-                                    width={100}
-                                    height={100}
-                                    className="object-cover rounded-t-md" />
-                                <div className="flex flex-row p-2 w-full">
-                                    <p className="text-lg font-bold">
-                                        {item.name}
-                                    </p>
-                                </div>
-                                <button
-                                    onClick={() => { cartContext.removeItem(item.id) }}
-                                    className="grid place-self-end m-1 w-fit text-gray-700 font-bold rounded-md hover:bg-red-300 hover:text-white text-xs transition">
-                                    <Image src="/trash.svg" alt="Cart" width={35} height={35} className="inline p-1" />
-                                </button>
-                            </div>
-                        ))
-                    }
+                <main className="flex flex-row h-[80vh] justify-center gap-20 bg-gray-200">
+                    <section className="max-h-142 flex flex-col gap-3 place-self-center bg-gray-50 shadow rounded-md min-w-1/2">
+                        <div className="p-5 border-b border-gray-300 font-bold">
+                            Tu compra
+                        </div>
+                        <div className="overflow-scroll overflow-x-hidden">
+                            {
+                                cartContext.cart.map((item) => (
+                                    <div key={item.id} className="flex flex-row pt-4 pb-4 pr-2 pl-2 gap-5 border-b border-gray-300">
+                                        <Image
+                                            src={item.images[0]}
+                                            alt={item.name}
+                                            width={100}
+                                            height={100}
+                                            className="object-cover rounded-md" />
+                                        <div className="flex flex-col p-2 justify-start">
+                                            <p className="text-md font-bold">
+                                                {item.name}
+                                            </p>
+                                            <button
+                                                onClick={() => { cartContext.removeItem(item.id) }}
+                                                className="p-2 text-blue-500 place-self-start font-bold rounded-md text-xs hover:underline">
+                                                Eliminar
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
 
-                    <form action="/api/checkout_sessions" method="POST">
-                        <section>
-                            <button type="submit" className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-blue-700">Checkout</button>
-                        </section>
-                        {
-                            cartContext.cart.map((item) => (
-                                <input key={item.id} type="hidden" name="price_ids" value={item.default_price} />
-                            ))
-                        }
-                    </form>
+                    </section>
+                    <section className="flex flex-col place-self-center bg-gray-50 shadow rounded-md min-w-1/4 min-h-1/4">
+                        <div className="p-5 border-b border-gray-300 font-bold">
+                            Resumen de compra
+                        </div>
+                        <div className="p-5 border-b border-gray-300 font-bold">
+                            Total a pagar:
+                        </div>
+                        <div className="p-3 place-self-center">
+                            <form action="/api/checkout_sessions" method="POST">
+                                <button type="submit" className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-blue-700">Continuar al pago</button>
+
+                                {
+                                    cartContext.cart.map((item) => (
+                                        <input key={item.id} type="hidden" name="price_ids" value={item.default_price} />
+                                    ))
+                                }
+                            </form>
+                        </div>
+                    </section>
                 </main>
                 <Footer />
             </>
