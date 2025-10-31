75e41b809b9407851ba6b32f649a7cd7dff64e1c
Javier Martínez <erick97am@gmail.com>
2025-10-30 18:49:03 -0600

    Simplify styles

--- app/ui/ContactForm.jsx
+++ app/ui/ContactForm.jsx
@@ -12,21 +12,21 @@ export function ContactForm() {
                 <form className="flex flex-col gap-3">
                     <div className="flex flex-col gap-1">
                         <label htmlFor="contact_name" className="text-xl font-medium">Nombre</label>
-                        <input id="contact_name" type="text" className="border-[1px] rounded-sm border-gray-300 min-h-8" />
+                        <input id="contact_name" type="text" className="border rounded-sm border-gray-300 min-h-8" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="email" className="text-xl font-medium">Email</label>
-                        <input id="email" type="email" className="border-[1px] rounded-sm border-gray-300 min-h-8" />
+                        <input id="email" type="email" className="border rounded-sm border-gray-300 min-h-8" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="telefono" className="text-xl font-medium">Telefono</label>
-                        <input id="telefono" type="tel" className="border-[1px] rounded-sm border-gray-300 min-h-8" />
+                        <input id="telefono" type="tel" className="border rounded-sm border-gray-300 min-h-8" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="mensaje" className="text-xl font-medium">Mensaje</label>
-                        <textarea id="mensaje" className="border-[1px] rounded-sm border-gray-300 min-h-10 resize-none"></textarea>
+                        <textarea id="mensaje" className="border rounded-sm border-gray-300 min-h-10 resize-none"></textarea>
                     </div>
-                    <button className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-gray-400 max-w-[10rem]">Enviar</button>
+                    <button className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-gray-400 max-w-40">Enviar</button>
                 </form>
             </div>
             <div className="flex flex-col">
