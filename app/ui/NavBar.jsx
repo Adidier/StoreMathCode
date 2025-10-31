c8608733b4f471dab91ab066adf4777cc715d1b0
Javier Martínez <erick97am@gmail.com>
2025-10-31 09:49:38 -0600

    Create SearchBar component

--- app/ui/NavBar.jsx
+++ app/ui/NavBar.jsx
@@ -1,33 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import { SearchBar } from "./SearchBar";
 
 export function NavBar() {
-  return (
-    <nav className="flex flex-col bg-stone-50 sticky top-0 z-10 shadow-md">
-      {/* <div className="grid grid-cols-2 font-light text-xs pt-2 pb-2 bg-sky-600 text-white p-2">
-                <div className="justify-self-center flex gap-7">
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/faq">PREGUNTAS FREQUENTES</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">TUTORIALES</Link>
-                </div>
-                <div className="justify-self-center flex gap-7">
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/portal">PORTAL</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">SUBIR COMPROBANTE</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">ESTATUS</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">PAGAR</Link>
-                </div>
-            </div> */}
-      <div className="flex flex-row gap-9 justify-center items-center font-light text-sm">
-        <Image src="/logoFlashback.webp" alt="Logo" width={253} height={91} className="ml-8 mr-8 mt-1.5 mb-1.5" />
-        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">INICIO</Link>
-        {/* <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/nosotros">NOSOTROS</Link> */}
-        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
-        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/servicios">SERVICIOS</Link>
-        {/* <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">EVENTOS SOCIALES</Link>
-                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/galeria">GALERIA</Link>
-                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">TIENDA</Link> */}
-        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/carrito">CARRITO</Link>
-        <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="flex flex-col bg-stone-50 sticky top-0 z-10 shadow-md">
+            <div className="flex flex-row gap-9 justify-center items-center font-light text-sm">
+                <Image src="/logoFlashback_Banner.webp" alt="Logo" width={177} height={64} className="ml-8 mr-8 mt-1.5 mb-1.5" />
+                <SearchBar />
+            </div>
+            <div className="flex flex-row justify-center font-light text-xs pt-2 pb-2 bg-sky-600 text-white p-2">
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">INICIO</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/servicios">SERVICIOS</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/carrito">CARRITO</Link>
+                <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
+            </div>
+        </nav>
+    );
 }
---
+++ app/ui/SearchBar.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Image from "next/image";
+
+export function SearchBar() {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleReturnKeyDown = (e) => {
+    if (!searchTerm.trim() === "") {
+      router.push("/contacto")
+    }
+  }
+
+  return (
+    <div className="flex flex-row w-1/4 justify-items-center">
+      <input
+        type="text"
+        placeholder="Buscar productos..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleReturnKeyDown}
+        className="border-l border-t border-b rounded-l-xs p-2 gap-2 border-gray-200 w-full outline-none" />
+      <button
+        onClick={() => {
+          if (!searchTerm.trim() === "") {
+            router.push("/contacto")
+          }
+        }}
+        className="p-2 border-r border-t border-b rounded-r-xs border-gray-200"
+      >
+        <Image src="/search-icon.svg" alt="search icon" width={20} height={20} />
+      </button>
+    </div>
+  )
+}
