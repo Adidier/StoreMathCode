023faa824d0dd8fdf896abae986f0b45eb85d8a0
Javier Martínez <erick97am@gmail.com>
2025-11-01 10:35:43 -0600

    Fix 'Contacto' Layout

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -2,12 +2,12 @@ import { Footer } from "../ui/Footer"
 import { ContactForm } from "../ui/ContactForm"
 
 export default function Contacto() {
-    return(
+    return (
         <>
-         <main>
-            <ContactForm/>
-        </main>
-        <Footer />
+            <main className="flex flex-col place-items-center items-center p-10 bg-gray-100">
+                <ContactForm />
+            </main>
+            <Footer />
         </>
     )
 }
