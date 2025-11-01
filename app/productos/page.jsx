e7eaca260c6bd6e818bc15bb262981a844cc8bfa
Javier Martínez <erick97am@gmail.com>
2025-11-01 10:33:50 -0600

    Add message for emtpy queries

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -6,13 +6,20 @@ import { Footer } from "../ui/Footer";
 export default async function Productos() {
     const productos = await fetchProductos();
     if (productos === null) {
-        return <div>Error al cargar los productos</div>
+        return (
+            <>
+                <main className="grid justify-items-center items-center bg-gray-100">
+                    <p className="rounded-md bg-gray-50 p-5 text-xl font-bold shadow-md">No tenemos productos disponibles</p>
+                </main>
+                <Footer />
+            </>
+        );
     } else {
         if (productos.length === 0) { return <div>No hay productos</div> }
         else {
             return (
                 <>
-                    <main className="grid place-items-center p-5">
+                    <main className="flex flex-col place-items-center bg-gray-100">
                         <CatalogView data={productos.data} />
                         <SpecialProductBanner />
                     </main>
