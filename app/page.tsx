4d9f5f4b9492a2f90257ea5a123390494cc03733
Javier Martínez <erick97am@gmail.com>
2025-11-01 10:33:50 -0600

    Add message for emtpy queries

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,20 @@ Amplify.configure(outputs);
 export default async function App() {
     const servicios = await fetchServicios();
     if (servicios === null) {
-        return <div>Error al cargar los productos</div>
+        return (
+            <>
+                <main className="grid justify-items-center items-center bg-gray-100">
+                    <p className="rounded-md bg-gray-50 p-5 text-xl font-bold shadow-md">No tenemos servicios disponibles</p>
+                </main>
+                <Footer />
+            </>
+        );
     } else {
         if (servicios.length === 0) { return <div>No hay servicios disponibles</div> }
         else {
             return (
                 <>
-                    <main className="grid place-items-center p-5">
+                    <main className="flex flex-col place-items-center bg-gray-100">
                         <CatalogView data={servicios.data} />
                     </main>
                     <Footer />
