854d47068e24028146354973cfa2e0a48f395d7c
Javier Martínez <erick97am@gmail.com>
2025-11-11 15:25:18 -0600

    Fix Special Products banner

--- app/ui/SpecialProductBanner.jsx
+++ app/ui/SpecialProductBanner.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export  async function SpecialProductBanner({ title = "Productos Especiales"}) {
   const servicios = await fetchServicios();
   return (
-    <div className="bg-gray-100 p-4 rounded-md shadow-md my-4 flex flex-row justify-items-center max-w-3/4 overflow-x-scroll gap-4">
+    <div className="bg-gray-100 p-4 rounded-md shadow-md my-4 flex flex-row justify-items-center w-2/4 overflow-x-scroll gap-4">
       {
         servicios.data.map((item) => (
           <div key={item.id} className="flex flex-col bg-gray-50 min-w-40 rounded-b-md justify-items-center shadow hover:shadow-md">
