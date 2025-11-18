ec145a78a20fd0e37943625275caa21a13e97688
Javier Martínez <erick97am@gmail.com>
2025-11-18 12:12:03 -0600

    Better sign up flow

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,17 +2,24 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { signUp, confirmSignUp } from 'aws-amplify/auth';
+import { signUp, confirmSignUp, signOut } from 'aws-amplify/auth';
+import clsx from 'clsx';
 
 export default function SignUp() {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmationCode, setConfirmationCode] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    hasError: false,
+    errorMessage: ""
+  });
   const [isWaitingConfirmation, setIsWaitingConfirmation] = useState(false);
 
   const handleSignUp = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const { isSignUpComplete, userId, nextStep } = await signUp({
         username: email,
@@ -25,14 +32,17 @@ export default function SignUp() {
       });
       if (nextStep.signUpStep === 'CONFIRM_SIGN_UP') {
         setIsWaitingConfirmation(true);
+        setLoading(false);
       }
     } catch (err) {
       console.log(err);
+      setLoading(false);
     }
   }
 
   const handleConfirmSignUp = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const { isSignUpComplete, nextStep } = await confirmSignUp({
         username: email,
@@ -40,10 +50,12 @@ export default function SignUp() {
       });
 
       if (nextStep.signUpStep === 'DONE') {
+        await signOut();
         router.push('/')
       }
     } catch (err) {
       console.error('Error signing up:', err);
+      setLoading(false);
     }
   }
 
@@ -56,12 +68,20 @@ export default function SignUp() {
             <form className="flex flex-col gap-3 p-10 pt-2">
               <div className="flex flex-col gap-1">
                 <label htmlFor="confirmationCode" className="text-xl font-medium">Código</label>
-                <input id="confirmationCode" type="text" className="border rounded-sm border-gray-300 min-h-8 p-2 text-xl font-bold" required autoComplete='off'
-                  onChange={(e) => setConfirmationCode(e.target.value)} value={confirmationCode}/>
+                <input
+                  id="confirmationCode"
+                  type="text"
+                  className="border rounded-sm border-gray-300 min-h-8 p-2 text-xl font-bold"
+                  required
+                  autoComplete='off'
+                  onChange={(e) => setConfirmationCode(e.target.value)} />
               </div>
-              <button onClick={(e) => handleConfirmSignUp(e)} className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-gray-400 max-w-40">Confirmar</button>
+              <button
+                onClick={(e) => handleConfirmSignUp(e)}
+                className={clsx("bg-black text-gray-50 font-bold px-4 py-2 max-w-40", loading && "bg-gray-400")}>
+                {loading ? 'Verificando...' : 'Confirmar'}
+              </button>
             </form>
-
           </div>
         </main>
       </>
@@ -77,14 +97,28 @@ export default function SignUp() {
             <form className="flex flex-col gap-3 p-10 pt-2">
               <div className="flex flex-col gap-1">
                 <label htmlFor="email" className="text-xl font-medium">Email</label>
-                <input id="email" type="email" className="border rounded-sm border-gray-300 min-h-8 p-2 text-md" required
+                <input
+                  id="email"
+                  type="email"
+                  className="border rounded-sm border-gray-300 min-h-8 p-2 text-md"
+                  required
                   onChange={(e) => setEmail(e.target.value)} />
               </div>
               <div className="flex flex-col gap-1">
                 <label htmlFor="password" className="text-xl font-medium">Contraseña</label>
-                <input id="password" type="password" className="border rounded-sm border-gray-300 min-h-8 p-2 text-md" required onChange={(e) => setPassword(e.target.value)} />
+                <input
+                  id="password"
+                  type="password"
+                  className="border rounded-sm border-gray-300 min-h-8 p-2 text-md"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
-              <button onClick={(e) => handleSignUp(e)} className="bg-black text-gray-50 font-bold px-4 py-2 rounded-md hover:bg-gray-400 max-w-40">Registrarse</button>
+              <button
+                onClick={(e) => handleSignUp(e)}
+                className={clsx("bg-black text-gray-50 font-bold px-4 py-2 max-w-40", loading && "bg-gray-400")}>
+                {loading ? 'Cargando...' : 'Registrarse'}
+              </button>
             </form>
           </div>
         </main>
