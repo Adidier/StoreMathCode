9a7119a0afd0974fb38595438bd9a3ae38de163c
Javier Martínez <erick97am@gmail.com>
2025-11-21 11:25:16 -0600

    Responsibe Navbar and footer

--- app/ui/Footer.jsx
+++ app/ui/Footer.jsx
@@ -2,77 +2,79 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
-    return (
-        <footer className="bg-black text-stone-50 border-t-6 border-sky-500 flex gap-10 flex-col items-center justify-center">
-            <div className="grid grid-cols-3 gap-10 pt-10 w-6xl justify-items-center">
-                <div>
-                    <header>
-                        <h1 className="text-xl mb-5">
-                            MENU
-                        </h1>
-                    </header>
-                    <div className="flex flex-col gap-2 font-extralight text-sm">
-                        <Link className="rounded-lg hover:text-amber-400" href="/">Inicio</Link>
-                        <Link className="rounded-lg hover:text-amber-400" href="/productos">Productos</Link>
-                        <Link className="rounded-lg hover:text-amber-400" href="/">Servicios</Link>
-                        <Link className="rounded-lg hover:text-amber-400" href="/contacto">Contacto</Link>
-                    </div>
-                </div>
-                <div>
-                    <header className="">
-                        <h1 className="text-xl mb-5">
-                            CLIENTES
-                        </h1>
-                    </header>
-                    <div className="flex flex-col font-extralight text-sm">
-                        {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/portal">Portal</Link> */}
-                        {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Comprobante</Link> */}
-                        {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Estatus</Link> */}
-                        {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Pago</Link> */}
-                    </div>
-                </div>
-                <div>
-                    <header>
-                        <h1 className="text-xl mb-5">
-                            CONTACTO
-                        </h1>
-                    </header>
-                    <div className="flex flex-col gap-2 font-extralight text-sm">
-                        <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="mailto:flashbacksuc1@gmail.com">
-                            <Image src="/mail_white.svg" alt="Logo" width={20} height={20} className="" />
-                            flashbacksuc1@gmail.com
-                        </Link>
-                        <Link className="rounded-lg flex flex-wrap gap-1 items-center" target="_blank" href="https://www.facebook.com/share/1CfShDMMYT/">
-                            <Image src="/facebook_white.svg" alt="Logo" width={20} height={20} className="" />
-                            Facebook
-                        </Link>
-                        <Link className="rounded-lg flex flex-wrap gap-1 items-center" target="_blank" href="https://www.instagram.com/flashback_pachuca/">
-                            <Image src="/instagram_white.svg" alt="Logo" width={20} height={20} className="" />
-                            Instagram
-                        </Link>
-                        {/* <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="/">
-                            <Image src="/youtube_white.svg" alt="Logo" width={20} height={20} className="" />
-                            YouTube
-                        </Link> */}
-                        <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="https://wa.me/527711072336">
-                            <Image src="/whatsapp_white.svg" alt="Logo" width={20} height={20} className="" />
-                            7717496737
-                        </Link>
-                        <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="tel:+527711072336">
-                            <Image src="/phone_white.svg" alt="Logo" width={20} height={20} className="" />
-                            7711072336
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <div className="grid grid-cols-2 items-center pb-7 w-6xl">
-                <Image src="/logoFlashback.webp" alt="Logo" width={253} height={91} className="ml-8 mr-8 mt-1.5 mb-1.5" />
-                <div className="flex flex-wrap font-bold text-sm">
-                    <Link className="p-1.5 rounded-lg" href="/terminos">Condiciones generales de venta</Link>
-                    <Link className="p-1.5 rounded-lg" href="/privacidad">Aviso de privacidad</Link>
-                    <Link className="p-1.5 rounded-lg" href="/terminos_paquetes">Términos y condiciones para paquetes de graduación</Link>
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer id="footer" className="bg-black text-stone-50 items-center justify-center">
+      <div id="navigation" className="hidden md:flex md:flex-row py-5 gap-10 justify-center">
+        <div className="px-10">
+          <header>
+            <h1 className="text-xl mb-5">
+              MENU
+            </h1>
+          </header>
+          <div className="flex flex-col gap-2 font-extralight text-sm">
+            <Link className="rounded-lg hover:text-amber-400" href="/">Inicio</Link>
+            <Link className="rounded-lg hover:text-amber-400" href="/nosotros">Nosotros</Link>
+            <Link className="rounded-lg hover:text-amber-400" href="/galeria">Galeria</Link>
+            <Link className="rounded-lg hover:text-amber-400" href="https://flashback-prod.d2xdk7rafd2luk.amplifyapp.com">Tienda</Link>
+            <Link className="rounded-lg hover:text-amber-400" href="/contacto">Contacto</Link>
+          </div>
+        </div>
+        <div className="px-10">
+          <header className="">
+            <h1 className="text-xl mb-5">
+              CLIENTES
+            </h1>
+          </header>
+          <div className="flex flex-col font-extralight text-sm">
+            {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/portal">Portal</Link> */}
+            {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Comprobante</Link> */}
+            {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Estatus</Link> */}
+            {/* <Link className="p-1.5 rounded-lg hover:text-gray-300" href="/">Pago</Link> */}
+          </div>
+        </div>
+        <div className="px-10">
+          <header>
+            <h1 className="text-xl mb-5">
+              CONTACTO
+            </h1>
+          </header>
+          <div className="flex flex-col gap-2 font-extralight text-sm">
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="mailto:flashbacksuc1@gmail.com">
+              <Image src="/mail_white.svg" alt="Logo" width={20} height={20} className="" />
+              flashbacksuc1@gmail.com
+            </Link>
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" target="_blank" href="https://www.facebook.com/share/1CfShDMMYT/">
+              <Image src="/facebook_white.svg" alt="Logo" width={20} height={20} className="" />
+              Facebook
+            </Link>
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" target="_blank" href="https://www.instagram.com/flashback_pachuca/">
+              <Image src="/instagram_white.svg" alt="Logo" width={20} height={20} className="" />
+              Instagram
+            </Link>
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="https://wa.me/527711072336">
+              <Image src="/whatsapp_white.svg" alt="Logo" width={20} height={20} className="" />
+              7717496737
+            </Link>
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="tel:+527711072336">
+              <Image src="/phone_white.svg" alt="Logo" width={20} height={20} className="" />
+              7711072336
+            </Link>
+            <Link className="rounded-lg flex flex-wrap gap-1 items-center" href="/contacto">
+              <Image src="/mail_white.svg" alt="Logo" width={20} height={20} className="" />
+              ¡Mandanos un mensaje!
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div id="termsAndLegal" className="flex flex-col md:flex-row p-2 md:pt-5 items-center justify-center md:gap-5">
+        <Image src="/logoFlashback_desktop.webp" alt="Logo" width={253} height={91} className="p-2 md:p-0 md:my-1.5" />
+        <div className="flex flex-wrap justify-around text-sm gap-2 md:text-sm">
+          <Link className="" href="/terminos">Condiciones generales de venta</Link>
+          <Link className="" href="/privacidad">Aviso de privacidad</Link>
+          <Link className="" href="/terminos_paquetes">Términos y condiciones para paquetes de graduación</Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
--- app/ui/NavBar.jsx
+++ app/ui/NavBar.jsx
@@ -2,31 +2,72 @@
 
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { SearchBar } from "./SearchBar";
 
 export function NavBar() {
-    const path = usePathname()
-    const adminRegex = /^\/admin/;
-    if (adminRegex.test(path)) {
-        return (
-            <></>
-        )
-    } else {
-        return (
-            <nav className="flex flex-col bg-stone-50 sticky top-0 z-10 shadow-md">
-                <div className="flex flex-row gap-9 justify-center items-center font-light text-sm">
-                    <Image src="/logoFlashback_Banner.webp" alt="Logo" width={177} height={64} className="ml-8 mr-8 mt-1.5 mb-1.5" />
-                    <SearchBar />
-                </div>
-                <div className="flex flex-row justify-center font-light text-xs pt-2 pb-2 bg-sky-600 text-white p-2">
-                    <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">SERVICIOS</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/carrito">CARRITO</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
-                    <Link className="p-1.5 rounded-lg hover:text-amber-400" href="https://production.d31fjrkeuz0102.amplifyapp.com">SITIO PRINCIPAL</Link>
-                </div>
-            </nav>
-        );
-    }
+  const path = usePathname()
+  const adminRegex = /^\/admin/;
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  useEffect(() => {
+    setMobileMenuVisible(false);
+  }, [path])
+
+  const MobileMenu = () => {
+    return (
+      <div id="mobileNavMenu" className="z-20 fixed top-0 flex flex-col bg-stone-50 h-full w-full">
+        <div id="mobileNavMenuHeader" className="flex flex-row justify-between items-center p-2">
+          <Image src="/logoFlashback_mobile.webp" alt="Logo" width={126} height={45} className="md:hidden" />
+          <button className="visible md:invisible mx-5" onClick={() => setMobileMenuVisible(!mobileMenuVisible)}>
+            <Image src="/close.svg" alt="Menu Button" width={16} height={16} />
+          </button>
+        </div>
+        <div id="mobileNvMenuLinks" className="font-bold text-lg flex flex-col px-5 py-2 gap-5">
+          <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">SERVICIOS</Link>
+          <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
+          <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/carrito">CARRITO</Link>
+          <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
+          <Link className="p-1.5 rounded-lg hover:text-amber-400" href="https://production.d31fjrkeuz0102.amplifyapp.com">SITIO PRINCIPAL</Link>
+        </div>
+      </div>
+    )
+  }
+  if (adminRegex.test(path)) {
+    return (
+      <></>
+    )
+  } else {
+    return (
+      <>
+        {
+          mobileMenuVisible
+            ?
+            <MobileMenu />
+            :
+            null
+        }
+        <nav className="z-10 shadow-md bg-stone-50 sticky top-0">
+          <div className="flex flex-row gap-9 justify-center items-center font-light p-2 md:p-0 text-sm">
+            <Link href="/">
+              <Image src="/logoFlashback_Banner.webp" alt="Logo" width={177} height={64} className="hidden md:block mx-8 my-1.5" />
+            </Link>
+            <Image src="/logoFlashback_mobile.webp" alt="Logo" width={126} height={45} className="md:hidden" />
+            <SearchBar />
+            <button className="visible md:invisible mx-5" onClick={() => setMobileMenuVisible(!mobileMenuVisible)}>
+              <Image src="/menu.svg" alt="Menu Button" width={16} height={32} />
+            </button>
+          </div>
+          <div className="hidden md:flex flex-row justify-center font-light text-xs pt-2 pb-2 bg-sky-600 text-white p-2">
+            <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/">SERVICIOS</Link>
+            <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/productos">PRODUCTOS</Link>
+            <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/carrito">CARRITO</Link>
+            <Link className="p-1.5 rounded-lg hover:text-amber-400" href="/contacto">CONTACTO</Link>
+            <Link className="p-1.5 rounded-lg hover:text-amber-400" href="https://production.d31fjrkeuz0102.amplifyapp.com">SITIO PRINCIPAL</Link>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
